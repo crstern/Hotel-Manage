d77b546cb438165b7eb944df1373296b74253df7
crstern <stern.cristi@gmail.com>
2020-12-28 13:16:00 +0200

    first commit

---
+++ Hotel Manage/Models/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HotelAPI.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public List<int> Reservations { get; set; }
+        public bool CurrentlyBooked { get; set; }
+        public int CurrentCustomerId { get; set; }
+        public List<int> Jobs { get; set; }
+    }
+
+    public class DbCtx : DbContext
+    {
+        public DbCtx() : base("DbConnectionString")
+        {
+            Database.SetInitializer<DbCtx>(new DropCreateDatabaseIfModelChanges<DbCtx>());
+        }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
---
+++ Hotel Manage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hotel_Manage.Startup))]
+namespace Hotel_Manage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
